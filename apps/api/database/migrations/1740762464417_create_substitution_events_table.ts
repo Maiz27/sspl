eefb205f5347a83d8295ae63c62b58e915eed9dc
Maged Faiz <magedfaiz98@gmail.com>
2025-02-28 19:15:57 +0200

    feat(api): add SubstitutionEvent model and migration with r/s

---
+++ apps/api/database/migrations/1740762464417_create_substitution_events_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'substitution_events'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+
+      table.uuid('match_id').references('id').inTable('matches').onDelete('CASCADE').notNullable()
+      table.uuid('team_id').references('id').inTable('teams').notNullable()
+
+      table.integer('minute').notNullable()
+      table.uuid('player_in_id').references('id').inTable('players').notNullable()
+      table.uuid('player_out_id').references('id').inTable('players').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
